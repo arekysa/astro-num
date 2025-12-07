efc4d8e2ccb6c803c18c24c19cd8e5c617aee83d
Aleksa Bošković <246548731+arekysa@users.noreply.github.com>
2025-12-07 13:04:54 +0100

    Update prvi_domaci.py

--- prvi_domaci.py
+++ prvi_domaci.py
@@ -1,6 +1,7 @@
 # fja vraca niz prvih n clanova fibonacija
 
 def fibonaci(n):
+    # izlazni slucaj za rekurziju
     if n == 0:
         return []
     if n == 1:
@@ -8,13 +9,14 @@ def fibonaci(n):
     if n == 2:
         return [0, 1]
 
-    stari = fibonaci(n-1)
-    return stari + [stari[-1]+stari[-2]]
+    stari = fibonaci(n-1) # uzima niz iz bazicnog slucaj [0, 1] i pocne da "razvija rekurziu unazad"
+    return stari + [stari[-1]+stari[-2]] # dodaje zbir poslednja dva na niz
+# fja za neki br n, pravi n-1 listu, doda novi elem.
+# zbirom poslednja dva, zatim tu novu listu vraca gore po rekurzivnom steku
 
-
-n = int(input("Unesite prirodan br veci od 0: "))
+n = int(input("Unesite prirodan br veci od 0: ")) # unos br n
 if n < 0:
-    print("Nije unet prirodan broj veci od nula!")
+    print("Nije unet prirodan broj veci od nula!") # izbacivanje greske i prekid programa
     exit()
 
-print(fibonaci(n))
+print(fibonaci(n)) # poziv fje i stampa
